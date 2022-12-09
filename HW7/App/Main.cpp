90b4c542016cc8b1caf1df50f24b3ac3a0e45dbf
David Wojciechowski <davidwojo15@gmail.com>
2022-12-09 13:15:31 -0800

    Additions to App project

---
+++ HW7/App/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Functions.h"
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Starting main()..." << std::endl;
+
+	func1();
+	return 0;
+}
---
+++ HW7/Lib/Functions.cpp
@@ -0,0 +1,8 @@
+#include "Functions.h"
+#include <iostream>
+
+int func1()
+{
+	std::cout << "Entering func1()..." << std::endl;
+	return 77;
+}
