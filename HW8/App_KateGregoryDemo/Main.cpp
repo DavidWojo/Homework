ed788375c4af0ebaa761d635d2ef5bbe0fbe2634
David Wojciechowski <davidwojo15@gmail.com>
2023-01-05 17:36:59 -0800

    updates

--- HW8/App_KateGregoryDemo/Main.cpp
+++ HW8/App_KateGregoryDemo/Main.cpp
@@ -2,59 +2,54 @@
 #include <string>
 #include <memory>
 
-
 class Resource
 {
 private:
 	std::string name;
 public:
-	Resource(const std::string n);
+	Resource(const std::string &n);
 	~Resource(void);
 	std::string GetName() const { return name; }
 };
 
-
-Resource::Resource(const std::string n) :name(n)
+Resource::Resource(const std::string &n) : 
+	name(n)
 {
 	std::cout << "constructing " << name << '\n';
 }
-
 
 Resource::~Resource(void)
 {
 	std::cout << "destructing " << name << '\n';
 }
 
-
 class Person
 {
 private:
-	std::string firstname;
-	std::string lastname;
+	std::string name;
 	int arbitrarynumber;
 	std::shared_ptr<Resource> pResource;
 
 public:
-	Person(const std::string first, const std::string last, int arbitrary);
+	Person(const std::string &n, int arbitrary);
 	std::string GetName() const;
 	int GetNumber() const { return arbitrarynumber; }
 	void SetNumber(int number) { arbitrarynumber = number; }
-	void SetFirstName(std::string first) { firstname = first; }
+	void SetName(std::string n) { name = n; }
 	void AddResource();
 	std::string GetResourceName() const { return pResource ? pResource->GetName() : ""; }
 };
 
-Person::Person(const std::string first, const std::string last,
-	int arbitrary) : firstname(first), lastname(last),
+Person::Person(const std::string &n, int arbitrary) :
+	name(n),
 	arbitrarynumber(arbitrary)
 {
 }
 
 std::string Person::GetName() const
 {
-	return firstname + " " + lastname;
+	return name;
 }
-
 
 void Person::AddResource()
 {
@@ -62,15 +57,11 @@ void Person::AddResource()
 	pResource = std::make_shared<Resource>("Resource for " + GetName());
 }
 
-
-
-
-
 int main(int argc, char *argv[])
 {
-	Person Kate("Kate", "Gregory", 345);
+	Person Kate("Kate", 345);
 	Kate.AddResource();
-	Kate.SetFirstName("Kate2");
+	Kate.SetName("Kate2");
 	Kate.AddResource();
 
 	Person Kate2 = Kate;
--- HW8/App_ManualMemoryManagement/Main.cpp
+++ HW8/App_ManualMemoryManagement/Main.cpp
@@ -13,50 +13,47 @@ public:
 };
 
 
-Resource::Resource(const std::string n) : name(n)
+Resource::Resource(const std::string n) :
+	name(n)
 {
 	std::cout << "constructing " << name << '\n';
 }
-
 
 Resource::~Resource(void)
 {
 	std::cout << "destructing " << name << '\n';
 }
 
-
-
 class Person
 {
 private:
-	std::string firstname;
-	std::string lastname;
+	std::string name;
 	int arbitrarynumber;
 	Resource* pResource;
 
 public:
-	Person(const std::string first, const std::string last, int arbitrary);
+	Person(const std::string &n, int arbitrary);
 	Person(const Person & p);
 	~Person();
 	Person& operator=(const Person & p);
 	std::string GetName() const;
 	int GetNumber() const { return arbitrarynumber; }
 	void SetNumber(int number) { arbitrarynumber = number; }
-	void SetFirstName(std::string first) { firstname = first; }
+	void SetName(std::string n) { name = n; }
 	void AddResource();
 	std::string GetResourceName() const { return pResource ? pResource->GetName() : ""; }
 };
 
-Person::Person(const std::string first, const std::string last,
-	int arbitrary) : firstname(first), lastname(last),
-	arbitrarynumber(arbitrary), pResource(nullptr)
+Person::Person(const std::string &n,	int arbitrary) :
+	name(n),
+	arbitrarynumber(arbitrary),
+	pResource(nullptr)
 {
 }
 
 Person::Person(const Person & p)
 {
-	firstname = p.firstname;
-	lastname = p.lastname;
+	name = p.name;
 	arbitrarynumber = p.arbitrarynumber;
 	pResource = new Resource(p.GetResourceName());
 }
@@ -69,19 +66,16 @@ Person::~Person()
 Person& Person::operator=(const Person & p)
 {
 	delete pResource;
-	firstname = p.firstname;
-	lastname = p.lastname;
+	name = p.name;
 	arbitrarynumber = p.arbitrarynumber;
 	pResource = new Resource(p.pResource->GetName());
 	return *this;
 }
 
-
 std::string Person::GetName() const
 {
-	return firstname + " " + lastname;
+	return name;
 }
-
 
 void Person::AddResource()
 {
@@ -89,14 +83,11 @@ void Person::AddResource()
 	pResource = new Resource("Resource for " + GetName());
 }
 
-
-
-
 int main(int argc, char * argv[])
 {
-	Person Kate("Kate", "Gregory", 345);
+	Person Kate("Kate", 345);
 	Kate.AddResource();
-	Kate.SetFirstName("Kate2");
+	Kate.SetName("Kate2");
 	Kate.AddResource();
 
 	Person Kate2 = Kate;
