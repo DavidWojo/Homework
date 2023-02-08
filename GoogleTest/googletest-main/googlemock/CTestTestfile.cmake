faa2a5def74d652c5d225e1aaa0889fc7a8e8e9d
David Wojciechowski <davidwojo15@gmail.com>
2023-02-07 19:54:06 -0800

    update

---
+++ GoogleTest/googletest-main/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/VisualStudio_Projects/VS2022_Projects/GoogleTest/googletest-main/googlemock
+# Build directory: C:/dev/VisualStudio_Projects/VS2022_Projects/GoogleTest/googletest-main/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ GoogleTest/googletest-main/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/VisualStudio_Projects/VS2022_Projects/GoogleTest/googletest-main/googletest
+# Build directory: C:/dev/VisualStudio_Projects/VS2022_Projects/GoogleTest/googletest-main/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
